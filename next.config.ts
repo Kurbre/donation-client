cca42f84fe0dc803654d7aa808a24c5fed91ce27
Kurbre <catiklou@gmail.com>
2026-04-17 12:39:46 +0300

    added rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	async rewrites() {
+		return [
+			{
+				// Все запросы с фронта на /api/... пойдут на твой сервер
+				source: '/api/:path*',
+				// Важно: если в переменной уже есть /api, здесь его не дублируй
+				destination: `https://donation-server-6j13.onrender.com/api/:path*`
+			}
+		]
+	}
+}
 
 export default nextConfig
