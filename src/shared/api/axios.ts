200a24694d0f2b9124e67596898fa31d083f75fc
Kurbre <catiklou@gmail.com>
2026-04-17 13:03:53 +0300

    update axios

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+const isServer = typeof window === 'undefined'
+
 export const axiosMain = axios.create({
-	baseURL: '/api',
+	baseURL: isServer ? process.env.NEXT_PUBLIC_API_URL : '/api',
 	withCredentials: true
 })
