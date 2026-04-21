be0d3ce18fab10d89d7206c6f8a8941adadb4819
Kurbre <catiklou@gmail.com>
2026-04-21 21:44:38 +0300

    fixed: logout

--- src/features/auth/logout/lib/useLogout.ts
+++ src/features/auth/logout/lib/useLogout.ts
@@ -7,6 +7,7 @@ import { toast } from 'react-toastify'
 export const useLogout = () => {
 	const router = useRouter()
 	const mutate = useMutation({
+		mutationKey: ['profile'],
 		mutationFn: logoutFetch,
 		onSuccess: data => {
 			toast.success(data.message)
